90f8046b3b8abf67e5e26c6150e8c08bc4feb818
dsolis421 <snapshot8d@gmail.com>
2017-02-25 11:59:08 -0600

    in progress v2, fully styled needs links

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
 $(document).ready(function(){
 
-var $skills = ['CSS3','JavaScript','jQuery','Bootstrap','HTML5'];
 var $bios = ['HUSBAND','GAMER','DOG LOVER','PROJECT MANAGER','DATA ANALYST','WEB DEVELOPER'];
 var $bioindex = 1;
-var $skillsindex = 1;
-
-/*function loopSkills() {
-  setInterval(function(){
-    $('#skill-carousel').fadeOut("slow","swing",function(){
-      $('#skill-carousel').html($skills[$skillsindex]);
-    });
-    $('#skill-carousel').fadeIn("slow","swing");
-    if ($skillsindex === 4) {
-      $skillsindex = 0;
-    } else {
-    $skillsindex++;
-    }
-  },5000)
-}*/
 
 function loopBios() {
   setInterval(function(){
@@ -34,7 +18,6 @@ function loopBios() {
 }
 
 loopBios();
-//loopSkills();
 
   var $scroll = $(document).scrollTop();
 

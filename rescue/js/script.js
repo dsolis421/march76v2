b47d9983cfab94ce0bb2cfd61172483d8ef88bed
dsolis421 <snapshot8d@gmail.com>
2017-06-24 11:21:10 -0500

    working v8 shelter list, working on pics

--- rescue/js/script.js
+++ rescue/js/script.js
@@ -5,7 +5,7 @@ function updateShelterStatus(message) {
   console.log(message);
   if(message) {
     $('#searchstatus').fadeOut("slow","swing", function() {
-      $('#searchstatus').html(`<h3>${message}</h3>`)
+      $('#searchstatus').html('<h3>' + message + '</h3>')
         .fadeIn("slow","swing");
     });
   } else {
@@ -15,17 +15,50 @@ function updateShelterStatus(message) {
   };
 };
 
+function evaluatePictures(photos) {
+  /*trying to ascertain if a usuable picture is available
+  otherwise display default value based on cat or dog*/
+  var photoslength = photos.length;
+  var goodphoto = '';
+  for(i = 0;i < photoslength;i++) {
+    if(photos[i][@size] === "t") {
+      goodphotos = photos[i];
+    }
+  };
+};
+
 function getShelter(id) {
   updateShelterStatus('Getting that family info...');
   $.getJSON($petfinderAPI + 'shelter.get?id=' + id + '&format=json&key=' + $devkey + '&callback=?')
     .done(function(shelter){
-      var shelterdetail = shelter.petfinder.shelter;
       console.log(shelter);
+      var shelterdetail = shelter.petfinder.shelter;
+      var sheltername = shelterdetail.name.$t;
+      var shelteraddress1 = shelterdetail.address1.$t ? shelterdetail.address1.$t : "Not available";
+      var shelteraddress2 = shelterdetail.address2.$t ? shelterdetail.address2.$t : "";
+      var sheltercity = shelterdetail.city.$t ? shelterdetail.city.$t : "";
+      var shelterstate = shelterdetail.state.$t ? shelterdetail.state.$t : "";
+      var shelterphone = shelterdetail.phone.$t ? shelterdetail.phone.$t : "Not available";
       $('#shelters').fadeOut("slow","swing", function() {
         $('#shelters').empty()
-          .html('<div class="shelterdetail"><h4>'+ shelterdetail.name.$t +
-            '</h4><h5>' + shelterdetail.email.$t + '</h5></div>')
-          .fadeIn("slow","swing");
+          .html('<div class="shelter-detail">\
+              <div class="shelter-header">\
+                <div class="shelter-name">\
+                  <h3>'+ sheltername +'</h3>\
+                </div>\
+                <div class="shelter-contact">\
+                  <h4>Address: ' + shelteraddress1  + ' ' +
+                    shelteraddress2 + ', ' +
+                    sheltercity + ', ' +
+                    shelterstate + '</h4>\
+                  <h4>Phone: ' + shelterphone + '</h4>\
+                  <h4>Email: ' + shelterdetail.email.$t + '</h4>\
+                </div>\
+              </div>\
+              <div class="shelter-pets">\
+              </div>\
+            </div>')
+          .fadeIn("slow","swing",getShelterPets(shelterdetail.id.$t));
       });
     })
     .done(function(){
@@ -50,12 +83,6 @@ function getSheltersZip(zip) {
             $('#shelters').append(listing);
           };
           $('#shelters').fadeIn("slow","swing");
-          /*$('#shelters').on("click", ".shelter", function(){
-            getShelter($(this).attr('shelterid'));
-            $('html, body').animate({
-              scrollTop: $('#adoption').offset().top - 35
-            }, 500);
-          });*/
           $('.shelter').on("click", function() {
             getShelter($(this).attr('shelterid'));
             $('html, body').animate({
@@ -76,6 +103,44 @@ function getSheltersZip(zip) {
     });
 };
 
+function getShelterPets(id) {
+  $.getJSON($petfinderAPI + 'shelter.getPets?id=' + id + '&output=full&format=json&key=' + $devkey + '&callback=?')
+    .done(function(petApiData){
+      console.log(petApiData);
+      //petfinder returns an object if only one pet exists, it returns an array of objects for multiple pets
+      if(petApiData.petfinder.pets.hasOwnProperty('pet') && Array.isArray(petApiData.petfinder.pets.pet)) {
+        console.log('found pets is an array');
+        var rescues = petApiData.petfinder.pets.pet;
+        for (x in rescues) {
+          var petdescription = rescues[x].description.$t ? rescues[x].description.$t.replace("'","\'") : "Not available";
+          $('shelter-pets').empty();
+          $('.shelter-pets').append('<div>\
+              <h4>' + rescues[x].name.$t + '</h4>\
+              <span>Sex: ' + rescues[x].sex.$t + '</span>\
+              <span>Breed: ' + rescues[x].breeds.breed.$t + '</span>\
+              <p>' + petdescription + '</p>\
+            </div>');
+        };
+      } else if (petApiData.petfinder.pets.hasOwnProperty('pet') && typeof petApiData.petfinder.pets.pet === 'object') {
+        console.log('found pet is an object');
+        var rescues = petApiData.petfinder.pets.pet;
+        $('shelter-pets').empty();
+        $('.shelter-pets').append('<div>\
+            <h4>' + rescues.name.$t + '</h4>\
+            <span>Sex: ' + rescues.sex.$t + '</span>\
+            <span>Breed: ' + rescues.breeds.breed.$t + '</span>\
+            <p>' + rescues.description.$t.replace("'","\'") + '</p>\
+          </div>');
+      } else {
+        console.log('looked for pets but none found');
+        $('.shelter-pets').append('h4>Looks like there are no pets currently at this shelter</h4>');
+      }
+    })
+    .error(function(err){
+      console.log('Get shelters by zip error! ' + err);
+    });
+}
+
 
 $(document).ready(function() {
 
@@ -95,8 +160,10 @@ $(document).ready(function() {
   });
 
   $('#sheltersearchgo').click(function(){
-    if($('#sheltersearch').val().length === 5) {
-      getSheltersZip($('#sheltersearch').val());
+    var zip = $('#sheltersearch').val()
+    if(zip.length === 5) {
+      console.log('searching ', zip);
+      getSheltersZip(zip);
     } else {
       updateShelterStatus('Oops! That doesn\'t look like a valid zip code.');
     }

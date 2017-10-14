75b2f45b88334f9e43bd933660a9721db5ff46bb
dsolis421 <danny@march76.com>
2017-10-13 21:05:48 -0500

    more redesigning

--- js/script.js
+++ js/script.js
@@ -1,73 +1,48 @@
-$(document).ready(function(){
-
-/*var $bios = ['WebDeveloper','Husband','Gamer','DogLover','ProjectManager','DataAnalyst'];
-var $bioindex = 1;*/
-
-var $quotes = [
-  {
-    quote: "I am always doing that which I cannot do, in order that I may learn how to do it.",
-    author: " - PabloPicasso"
-  },
-  {
-    quote: "A dog is the only thing on earth that loves you more than you love yourself.",
-    author: " - JoshBillings"
-  },
-  {
-    quote: "Happiness lies in the joy of achievement and the thrill of creative effort.",
-    author: " - FranklinDRoosevelt"
-  },
-  {
-    quote: "Quality is never an accident. It is always the result of intelligent effort.",
-    author: " - JohnRuskin"
-  }
-];
-
-/*function loopBios() {
-  setInterval(function(){
-    $('#bio-carousel').fadeOut(1200,"swing",function(){
-      $('#bio-carousel').html($bios[$bioindex]);
-    });
-    $('#bio-carousel').fadeIn(1200,"swing");
-    if ($bioindex === 5) {
-      $bioindex = 0;
-    } else {
-    $bioindex++;
+$(document).ready(function() {
+  var $scroll = $(document).scrollTop();
+  var $quotes = [{
+      quote: "I am always doing that which I cannot do, in order that I may learn how to do it.",
+      author: " - PabloPicasso"
+    },
+    {
+      quote: "A dog is the only thing on earth that loves you more than you love yourself.",
+      author: " - JoshBillings"
+    },
+    {
+      quote: "Happiness lies in the joy of achievement and the thrill of creative effort.",
+      author: " - FranklinDRoosevelt"
+    },
+    {
+      quote: "Quality is never an accident. It is always the result of intelligent effort.",
+      author: " - JohnRuskin"
     }
-  },5000)
-}*/
+  ];
 
-function pickQuote() {
-  var i = Math.floor(Math.random() * 4);
-  var $myquote = $quotes[i];
-  $('#myquote').html($myquote.quote + $myquote.author);
-}
+  function pickQuote() {
+    var i = Math.floor(Math.random() * 4);
+    var $myquote = $quotes[i];
+    $('#myquote').html($myquote.quote + $myquote.author);
+  }
 
+  pickQuote();
 
-
-//loopBios();
-pickQuote();
-
-var $scroll = $(document).scrollTop();
-
-function STop() {
-    console.log("scroll is " + $scroll);
-}
-
-$(document).scroll(function(){
+  $(document).scroll(function() {
     $scroll = $(document).scrollTop();
-    if ($scroll > 150) {
-        $('#mar-nav').fadeIn(500,"swing");
-    } else if ($scroll == 0) {
-        $('#mar-nav').fadeOut(500,"swing");
+    if ($scroll > 250) {
+      $('.fa-chevron-down').fadeIn(500, "swing");
+    } else if ($scroll < 230) {
+      $('.fa-chevron-down').fadeOut(500, "swing");
     }
-});
+  });
 
-$('nav .fa-chevron-down, .toggle-menu a').click(function(){
-  if($('.toggle-menu').hasClass('collapsed')){
-    $('.toggle-menu').removeClass('collapsed');
-  } else {
-    $('.toggle-menu').addClass('collapsed');
-  }
-});
+  $('nav .fa-chevron-down, .toggle-menu a').click(function() {
+    if ($('.toggle-menu').hasClass('collapsed')) {
+      $('.toggle-menu').removeClass('collapsed');
+      $('.toggle-menu').fadeIn();
+    } else {
+      $('.toggle-menu').addClass('collapsed');
+      $('.toggle-menu').fadeOut();
+    }
+  });
 
 });
